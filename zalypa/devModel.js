1386d7cc8651da094d94ba92738321e60560029c
yguryn <69647986+yguryn@users.noreply.github.com>
2023-12-04 13:27:56 +0200

    Add files via upload

--- zalypa/devModel.js
+++ zalypa/devModel.js
@@ -1,16 +1,15 @@
 function getDeviceModel() {
     var userAgent = navigator.userAgent;
 
-    // Regular expression to extract device model
-    // This is a basic example and might need to be adjusted based on the variety of user agents
-    var regExp = /(?:\(([^)]+)\))/;
-    var matches = regExp.exec(userAgent);
-
-    if (matches && matches[1]) {
-        return matches[1].split(';')[1].trim(); // Adjust this part based on your needs
-    } else {
-        return "Unknown Device";
-    }
+    // var regExp = /(?:\(([^)]+)\))/;
+    // var matches = regExp.exec(userAgent);
+    //
+    // if (matches && matches[1]) {
+    //     return matches[1].split(';')[1].trim();
+    // } else {
+    //     return "Unknown Device";
+    // }
+    return userAgent
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
